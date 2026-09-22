403307b533f596ef4d93be7442f296fcaf3174d0
SerpentSveta <sveta.aleksandrova@gmail.com>
2026-09-22 14:22:00 +0300

    fix: vite.config change

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/rsschool-landing-page/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        menu: 'menu.html',
+      },
+    },
+  },
 });
