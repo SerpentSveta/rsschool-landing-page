f305d6506fc29961583dc71cc2e4d9dded754d60
SerpentSveta <sveta.aleksandrova@gmail.com>
2026-09-22 21:43:17 +0300

    feat: add toggle theme

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,25 @@ const heroVideo = document.querySelector('.hero__video');
 if (heroVideo) {
   heroVideo.play();
 }
+
+const themeSwitch = document.querySelector('.theme-switch');
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+  document.documentElement.dataset.theme = 'dark';
+}
+
+if (themeSwitch) {
+  themeSwitch.addEventListener('click', () => {
+    const isDark = document.documentElement.dataset.theme === 'dark';
+
+    if (isDark) {
+      document.documentElement.removeAttribute('data-theme');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.documentElement.dataset.theme = 'dark';
+      localStorage.setItem('theme', 'dark');
+    }
+  });
+}
